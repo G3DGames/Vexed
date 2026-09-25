bcab34fe3291444da9a5b8675aae163b568de2c2
peardox <simon@peardox.com>
2026-09-25 18:48:09 +0100

    Add IMAGE_FILE_LARGE_ADDRESS_AWARE for Win32

--- Vexed.dpr
+++ Vexed.dpr
@@ -4,9 +4,19 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   VexedMain in 'src\VexedMain.pas' {Form1},
+{$if defined(MSWINDOWS)}
   DisplayData in 'common\DisplayData.pas',
   GpuPreference in 'common\GpuPreference.pas',
+  {$if defined(WIN32)} // For IMAGE_FILE_LARGE_ADDRESS_AWARE
+  Windows,
+  {$ifend}
+{$ifend}
   TileMapRenderer in 'src\TileMapRenderer.pas';
+
+// Win32 ONLY needs IMAGE_FILE_LARGE_ADDRESS_AWARE
+{$if defined(MSWINDOWS) and defined(WIN32)}
+{$SetPEFlags IMAGE_FILE_LARGE_ADDRESS_AWARE}
+{$ifend}
 
 {$R *.res}
 
